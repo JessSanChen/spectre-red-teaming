92f2f00afc29c24bd206da3afa3474a7598f9a3d
Jessica Chen <jessicachen@Jessicas-MacBook-Pro-6.local>
2024-11-15 18:15:30 -0500

    jessica integrates

--- backend/app/routers/client.py
+++ backend/app/routers/client.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter, status
+import requests
+
+YOUR_API_KEY = "Y2VudGNvbTpsZXRtZWlu"
+url = "https://hackathon.niprgpt.mil/llama/v1/chat/completions"
+headers = {
+    "Authorization": "Bearer " + YOUR_API_KEY,
+    "Content-Type": "application/json"
+}
 
 router = APIRouter()
 
@@ -10,13 +18,87 @@ async def healthCheck():
 async def getOriginalResponse(request: str):
 
     # TODO: call LLM and return the raw response
-    return "Placeholder " + request
+    transformed_prompt_data = {
+        "model": "neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8",
+        "messages": [{"role": "user", "content": request}],
+        "temperature": 0.7
+    }
+    llm_response = requests.post(url, headers=headers, json=transformed_prompt_data).json()
+    llm_output = llm_response['choices'][0]['message']['content']
+    print("LLM Output: "+llm_output)
+    return llm_output
 
 @router.get("/get-jailbroken-response", tags=['client'], status_code=status.HTTP_200_OK)
 async def getJailbrokenResponse(request: str):
     # TODO: transform request to jailbroken request
+    def categorize(request):
+        with open("categorize.txt", 'r') as file:
+            categorize_prompt = file.read()
+        input_data = {
+            "model": "neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8",
+            "messages": [{"role": "user", "content": categorize_prompt + "\n input is: " + request}],
+            "temperature": 0
+        }
+        response = requests.post(url, headers=headers, json=input_data).json()
+        result = response['choices'][0]['message']['content']
+        print("Category: " + result)
+        # "Hazardous Chemicals" or "Guns and Illegal Weapons"
+        return result
+
+    def language_obf(request):
+        # extract specific name
+        with open("obf_input.txt", 'r') as file:
+            mailicious_prompt = file.read()
+        obf_input_data = {
+            "model": "neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8",
+            "messages": [{"role": "user", "content": mailicious_prompt + "\n input is: " + request}],
+            "temperature": 0
+        }
+        obf_response = requests.post(url, headers=headers, json=obf_input_data).json()
+        obf_result = obf_response['choices'][0]['message']['content']
+        print("Name obfuscation result: " + obf_result)
+        return obf_result
+
+    def purpose_obf(request):
+        with open("purpose2.txt", 'r') as file:
+            mailicious_prompt = file.read()
+        transformed_prompt_data = {
+            "model": "neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8",
+            "messages": [{"role": "user", "content": mailicious_prompt + "\n input is: "+ request}],
+            "temperature": 0.7
+        }
+        response = requests.post(url, headers=headers, json=transformed_prompt_data).json()
+        result = response['choices'][0]['message']['content']
+        print(result)
+        return result
+
+    def judge(llm_output):
+        return len(llm_output) > 300
+
+    def transform(request):
+        category = categorize(request)
+        if category == "Hazardous Chemicals":
+            # 1st round
+            purpose_output = purpose_obf(request)
+            purpose_result = judge(purpose_output)
+            if purpose_result:
+                print("Purpose rendering successfully jailbroke this LLM.")
+                return purpose_output
+            
+            print("Purpose rendering was not enough. We must agentically use another method.")
+            # 2nd layer output
+            lang_output = language_obf(purpose_output)
+            lang_result = judge(lang_output)
+            if lang_result:
+                print("Language obfuscation successfully jailbroke this LLM.")
+                return lang_output
+            
+            print("FAILED TO JAILBREAK")
+        else: # "Guns and Illegal Weapons"
+            raise NotImplementedError()
 
     # TODO: call LLM with jailbroken request
 
     # TODO: return jailbroken response
-    return "Jailbreak placeholder " + request
+    result = transform(request)
+    return result
